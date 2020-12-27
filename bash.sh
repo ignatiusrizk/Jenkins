e01bcf405900c3080bdc6832e00f0a78d87225e1
Ignatius Rizky <46715608+ignatiusrizk@users.noreply.github.com>
2020-12-27 11:05:28 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,12 +1,12 @@
 echo "<!DOCTYPE html>
 <html>
 <head>
-  <title>Name of the website</title>
+  <title>Harpy</title>
 </head>
 
 <body>
-  <h1>Heading for the content below</h1>
-  <p>Text text text</p>
+  <h1>IDN Mantab/h1>
+  <p>Upgrade Your Skill, Upgrade Your Life!</p>
 </body>
 
 </html>" > index.html
